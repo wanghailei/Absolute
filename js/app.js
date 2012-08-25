6ba51c11cc5e5287f74469c1733082725e5de3d0
Hailei Wang <wanghailei@farefore.com>
2012-08-25 15:50:35 +0800

    A clean fullfill UI framework code base.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+	
+	$(".scroll-pane").jScrollPane({
+			showArrows: true,
+			horizontalArrowPositions: 'after'
+	});
+	layout();
+});
+$(window).resize(function() {
+	layout();
+});
+
+function layout() {
+	$(".plate").height( $(window).height()-160 );
+}
